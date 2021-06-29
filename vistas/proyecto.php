afccc3edada2da0973df5b8e3714833004ec05d2
Wicho Zambrano <luis.eduardo.zambrano.ramos@gmail.com>
2021-06-28 20:33:10 -0500

    Update proyecto.php

--- vistas/proyecto.php
+++ vistas/proyecto.php
@@ -33,6 +33,16 @@ for ($i = 2; $i < count($arrayArchivos); $i++) {
     $ex = explode(" .", $nombreAux);
     $nombre[$i - 2] = $ex[0];
 }
+
+session_start();
+        if(!ISSET($_SESSION['correo'])){
+            header('location:index.php');
+        }else{
+            if((time() - $_SESSION['time']) > 930){
+                header('location: logout.php');
+            }
+        }
+}
 ?>
 
 <!DOCTYPE html>
@@ -72,7 +82,11 @@ for ($i = 2; $i < count($arrayArchivos); $i++) {
 </head>
 
 <body class="body">
-
+        <?php
+            require 'conexion.php';
+            $query = $conn->query("SELECT * FROM `usuarios` WHERE `correo` = '$_SESSION[correo]'");
+            $fetch = $query->fetch_array();
+        ?>
     <div class="container">
         <div class="row">
             <div class="borde col-sm-13 width:100%">
