71e849b80577974643616fee5b6e5c93b2f875e2
EduardoVeco <84112266+EduardoVeco@users.noreply.github.com>
2021-06-29 18:12:32 -0500

    proposito

--- js/registroequipo1.js
+++ js/registroequipo1.js
@@ -192,7 +192,8 @@ propositoForm.addEventListener('submit', e => {
             if (campos.proposito) {
                 console.log('Aqui ya pasaste a la otra ventana carnal')
                 console.log(document.getElementById('proposito').value)
-                window.location = "formarequipo.php?noControlA=" + noControlA + '&nombre=' + nombre + '&paterno=' + paterno + '&materno=' + materno + '&folio=' + folio;;
+                propositoFinal = document.getElementById('proposito')
+                window.location = "formarequipo.php?noControlA=" + noControlA + '&nombre=' + nombre + '&paterno=' + paterno + '&materno=' + materno + '&folio=' + folio + '&proposito=' + propositoFinal;
                 if (campos.nombre && campos.paterno && campos.materno) {
                     //  noControl.push(campos.nc)
                     //  nombre.push(document.getElementById('nombre').value)
