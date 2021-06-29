de172f7b65143fbb4dbbe7b909ea872200578dea
Wicho Zambrano <luis.eduardo.zambrano.ramos@gmail.com>
2021-06-28 20:33:54 -0500

    Update registroequipo1.php

--- vistas/registroequipo1.php
+++ vistas/registroequipo1.php
@@ -17,6 +17,15 @@ print_r($materno);
 if (empty($noControl)) {
     header("Location:registroequipo.php");
 }
+session_start();
+        if(!ISSET($_SESSION['correo'])){
+            header('location:index.php');
+        }else{
+            if((time() - $_SESSION['time']) > 930){
+                header('location: logout.php');
+            }
+        }
+}
 ?>
 
 <!DOCTYPE html>
@@ -48,7 +57,11 @@ if (empty($noControl)) {
 </head>
 
 <body class="body">
-
+        <?php
+            require 'conexion.php';
+            $query = $conn->query("SELECT * FROM `usuarios` WHERE `correo` = '$_SESSION[correo]'");
+            $fetch = $query->fetch_array();
+        ?>
     <div class="container">
         <div class="row">
             <div class="borde col-sm-13 width:100%">
