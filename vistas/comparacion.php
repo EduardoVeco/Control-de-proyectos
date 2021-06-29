76391281f56ccf20aed736eca19652b6d5ba8f82
Wicho Zambrano <luis.eduardo.zambrano.ramos@gmail.com>
2021-06-28 20:32:16 -0500

    Update comparacion.php

--- vistas/comparacion.php
+++ vistas/comparacion.php
@@ -1,5 +1,14 @@
 <?php
 $conexion = mysqli_connect('localhost', 'root', '', 'controlproyectos');
+session_start();
+        if(!ISSET($_SESSION['correo'])){
+            header('location:index.php');
+        }else{
+            if((time() - $_SESSION['time']) > 930){
+                header('location: logout.php');
+            }
+        }
+}
 ?>
 
 
@@ -25,7 +34,11 @@ $conexion = mysqli_connect('localhost', 'root', '', 'controlproyectos');
 </head>
 
 <body class="body">
-
+        <?php
+            require 'conexion.php';
+            $query = $conn->query("SELECT * FROM `usuarios` WHERE `correo` = '$_SESSION[correo]'");
+            $fetch = $query->fetch_array();
+        ?>
     <div class="container">
         <div class="row">
             <div class="borde col-sm-13 width:100%">
