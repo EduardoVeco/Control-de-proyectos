72d21906cab090d984b1404f3fa168034cdd1bec
Wicho Zambrano <luis.eduardo.zambrano.ramos@gmail.com>
2021-06-28 20:33:38 -0500

    Update registrodueno.php

--- vistas/registrodueno.php
+++ vistas/registrodueno.php
@@ -19,6 +19,15 @@ print_r($alcances);
 print_r($resumen);
 print_r($correo);
 
+session_start();
+        if(!ISSET($_SESSION['correo'])){
+            header('location:index.php');
+        }else{
+            if((time() - $_SESSION['time']) > 930){
+                header('location: logout.php');
+            }
+        }
+}
 
 ?>
 
@@ -44,7 +53,11 @@ print_r($correo);
 </head>
 
 <body class="body">
-
+        <?php
+            require 'conexion.php';
+            $query = $conn->query("SELECT * FROM `usuarios` WHERE `correo` = '$_SESSION[correo]'");
+            $fetch = $query->fetch_array();
+        ?>
     <div class="container">
         <div class="row">
             <div class="borde col-sm-13 width:100%">
