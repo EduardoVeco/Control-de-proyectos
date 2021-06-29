9ef5c47376b8edccea02028d8a3f779789987c6b
Wicho Zambrano <luis.eduardo.zambrano.ramos@gmail.com>
2021-06-28 20:31:52 -0500

    Update asesor.php

--- vistas/asesor.php
+++ vistas/asesor.php
@@ -2,6 +2,15 @@
 $correo = $_REQUEST['correo'];
 $conexion = mysqli_connect('localhost', 'root', '', 'controlproyectos');
 
+session_start();
+        if(!ISSET($_SESSION['correo'])){
+            header('location:index.php');
+        }else{
+            if((time() - $_SESSION['time']) > 930){
+                header('location: logout.php');
+            }
+        }
+?>
 ?>
 
 <!DOCTYPE html>
@@ -40,7 +49,11 @@ $conexion = mysqli_connect('localhost', 'root', '', 'controlproyectos');
 </head>
 
 <body class="body">
-
+        <?php
+            require 'conexion.php';
+            $query = $conn->query("SELECT * FROM `usuarios` WHERE `correo` = '$_SESSION[correo]'");
+            $fetch = $query->fetch_array();
+        ?>
     <div class="container">
         <div class="row">
             <div class="borde col-sm-13 width:100%">
