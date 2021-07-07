2d71d3f5ada2ac68fd4b53689a8a15b0a2063ddd
EduardoVeco <84112266+EduardoVeco@users.noreply.github.com>
2021-07-06 19:00:56 -0500

    cerrrar sesion

--- vistas/asesor.php
+++ vistas/asesor.php
@@ -78,6 +78,7 @@ if (!isset($_SESSION['correo'])) {
                         </a>
                         <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
                             <a class="dropdown-item" href="cambiarcontrasena.php?correo=<?php echo $correo ?>&estado=0">Cambiar contraseña</a>
+                            <a class="dropdown-item" href="index.html">Cerrar sesion</a>
                         </div>
                     </li>
                 </ul>
--- vistas/dptoinvestigacion.php
+++ vistas/dptoinvestigacion.php
@@ -75,6 +75,7 @@ if (!isset($_SESSION['correo'])) {
                         </a>
                         <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
                             <a class="dropdown-item" href="cambiarcontrasena.php?correo=<?php echo $correo ?>">Cambiar contraseña</a>
+                            <a class="dropdown-item" href="index.html">Cerrar sesion</a>
                         </div>
                     </li>
                 </ul>
